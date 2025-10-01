df61151641cd565ce9f48e503e4b26c4fe29f3b4
DAI-KON <d2a7d2al7ps@gmail.com>
2025-10-01 23:21:13 +0900

    Finish

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,7 @@
 
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,10 +13,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-		paths: {
-			base: dev ? '' : '/ChromeMainPage'
-		}
+		adapter: adapter()
 	}
 };
 
